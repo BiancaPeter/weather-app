466a99b3c4f02de2ae22e56fd9c964b9f4317d7f
BiancaPeter <botabiancateodora@gmail.com>
2022-11-23 00:17:09 +0200

    first commit

---
+++ src/main/java/com/spring/weatherapp/service/UserService.java
@@ -0,0 +1,10 @@
+package com.spring.weatherapp.service;
+
+import com.spring.weatherapp.repository.UserRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
